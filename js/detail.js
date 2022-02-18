cbfe596e7c5f740c04545e05925e72873bd1c15f
Allawi465 <allawi465@gmail.com>
2022-02-19 00:40:14 +0100

    .

--- js/detail.js
+++ js/detail.js
@@ -1,4 +1,4 @@
-const resultsContainer = document.querySelector(".container");
+const resultsContainer = document.querySelector(".deital-data");
 
 const queryString = document.location.search;
 
@@ -8,25 +8,28 @@ const id = params.get("id");
 
 console.log(id);
 
-const url = "https://breakingbadapi.com/api/characters/";
+const url = "https://breakingbadapi.com/api/characters/" + id;
 
 async function getNames() {
     try {
         const response = await fetch(url);
-
         const data = await response.json();
 
-        console.log(data);
+        const name = data;
 
-        for (let i = 0; i < data.length; i++) {
+        console.log(name);
 
-            if (i === 10) {
-              break;
-           }
+        name.forEach(function(name) {
+            resultsContainer.innerHTML = `<div>
+            <div class="details-image" style="background-image: url('${name.img}')"></div>
+            <h1>${name.name}</h1>
+            <p>${name.category}</p> 
+            <p>Nickname: ${name.nickname}</p>
+            <p>${name.occupation[0]}</p>
+            </div>`;
+        })
+
     
-            resultsContainer.innerHTML += `<div> ${data[i].char_id}</div>`;
-        }
-
     } catch (error) {
         console.log(error);
     }
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const dropdown = document.querySelector("select");
 const resultsContainer = document.querySelector(".list");
+const url = "https://breakingbadapi.com/api/characters/";
 
 dropdown.onchange = function() {
     console.log(event.target.value);
@@ -11,9 +12,30 @@ dropdown.onchange = function() {
     setTimeout(function() {
         resultsContainer.innerHTML = " ";
         for(let i = 1; i <= selectedValue; i++) {
-            resultsContainer.innerHTML += `<a href="detail.html"?id="" class="item">${i}</a>`;
+            resultsContainer.innerHTML = `<a href="detail.html?id=" class="item">${i}</a>`;
         }
 
     }, 2000);
 
 };
+
+async function getNames() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const name = data;
+
+        console.log(data);
+
+        name.forEach(function(name) {
+            resultsContainer.innerHTML += `<a href="detail.html?id=${name.char_id}" class="item">${name.char_id}</a>`;
+        })
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getNames()
